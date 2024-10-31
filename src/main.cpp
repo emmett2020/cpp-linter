cf6ce0f232bd8d167b0f8c37e13c29f09c5f7c7c
root <emmettzhang2020@outlook.com>
2024-10-31 05:39:22 +0000

    compiled with v2

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
-#include <boost/process.hpp>
+#define BOOST_PROCESS_V2_SEPARATE_COMPILATION 1
+
+#include <boost/process/v2/bind_launcher.hpp>
+#include <boost/process/v2/cstring_ref.hpp>
+#include <boost/process/v2/default_launcher.hpp>
+#include <boost/process/v2/environment.hpp>
+#include <boost/process/v2/error.hpp>
+#include <boost/process/v2/execute.hpp>
+#include <boost/process/v2/exit_code.hpp>
+#include <boost/process/v2/pid.hpp>
+#include <boost/process/v2/popen.hpp>
+#include <boost/process/v2/process.hpp>
+#include <boost/process/v2/process_handle.hpp>
+#include <boost/process/v2/shell.hpp>
+#include <boost/process/v2/src.hpp>
+#include <boost/process/v2/start_dir.hpp>
+#include <boost/process/v2/stdio.hpp>
 #include <iostream>
 
 using namespace std;
-using namespace boost::process;
+using namespace boost::process::v2;
+// using namespace boost;
 
 int main() {
-  ipstream pipe_stream;
-  child c("gcc --version", std_out > pipe_stream);
+  boost::asio::io_context ctx;
 
-  std::string line;
-
-  while (pipe_stream && std::getline(pipe_stream, line) && !line.empty()) {
-    std::cerr << line << std::endl;
-  }
-
-  c.wait();
+  auto proc = process{ctx, "/bin/ls", {}};
+  assert(proc.wait() == 0);
 }
