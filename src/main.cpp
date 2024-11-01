e3d72a179efaced8c9e4b3f2737b84384a6420a5
emmett <emmettzhang2020@outlook.com>
2024-11-01 12:41:31 +0000

    temp save

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <boost/asio/error.hpp>
 #include <boost/asio/read.hpp>
 #include <boost/process/v2.hpp>
+#include <boost/process/v2/cstring_ref.hpp>
 #include <boost/process/v2/src.hpp>
 
 #include <boost/throw_exception.hpp>
@@ -19,14 +20,15 @@ struct CommandResult {
   std::string std_err;
 };
 
-CommandResult Exec(std::string_view command,
+CommandResult Exec(std::string_view command_path,
                    std::initializer_list<bp::string_view> args,
                    std::unordered_map<std::string, std::string> env = {}) {
   boost::asio::io_context ctx;
   boost::asio::readable_pipe rp_out{ctx};
   boost::asio::readable_pipe rp_err{ctx};
 
-  bp::process proc(ctx, command, args, bp::process_stdio{{}, rp_out, rp_err},
+  bp::process proc(ctx, command_path, args,
+                   bp::process_stdio{{}, rp_out, rp_err},
                    bp::process_environment{env});
 
   boost::system::error_code ec;
@@ -34,13 +36,13 @@ CommandResult Exec(std::string_view command,
   boost::asio::read(rp_out, boost::asio::dynamic_buffer(res.std_out), ec);
   if (ec && ec != boost::asio::error::eof) {
     throw std::runtime_error(std::format(
-        "Read stdout faild: {} in executing {}", ec.message(), command));
+        "Read stdout faild: {} in executing {}", ec.message(), command_path));
   }
   ec.clear();
   boost::asio::read(rp_err, boost::asio::dynamic_buffer(res.std_err), ec);
   if (ec && ec != boost::asio::error::eof) {
     throw std::runtime_error(std::format(
-        "Read stderr faild: {} in executing {}", ec.message(), command));
+        "Read stderr faild: {} in executing {}", ec.message(), command_path));
   }
   res.exit_code = proc.wait();
   return res;
@@ -48,7 +50,7 @@ CommandResult Exec(std::string_view command,
 
 int main() {
   auto [exit_code, std_out, std_err] =
-      Exec("/usr/bin/c++", {"--version"}, {{"CXX", "/usr/bin/g++-14"}});
+      Exec("/linter/exe", {""}, {{"ADD", "HI"}});
   std::print("{}\n", exit_code);
   std::print("{}\n", std_out);
   std::print("{}\n", std_err);
