1acde16bc1681ff05055f99c9434c41e813b723e
root <emmettzhang2020@outlook.com>
2024-11-02 16:50:21 +0000

    basic

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
+#include <range/v3/all.hpp>
+
 #include "utils/shell.h"
 #include "tools/clang_tidy.h"
 
+#include <cctype>
 #include <iostream>
 
 using namespace linter;
 
 int main() {
-  std::cout << GetClangToolFullPath("clang-tidy", "20");
+  auto cmd = GetClangToolFullPath("clang-tidy", "20");
+  std::cout << cmd.size() << "\n";
+  auto c = cmd
+         | ranges::views::reverse
+         | ranges::views::drop_while(::isspace)
+         | ranges::views::reverse
+         | ranges::to<std::string>();
+  std::cout << c << std::endl;
+  RunClangTidy(c, "t.cpp", "", false);
 }
