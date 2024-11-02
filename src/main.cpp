ced19381759bceb2e3ec5f2872273860748ceb0d
root <emmettzhang2020@outlook.com>
2024-11-02 16:37:36 +0000

    hi

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,10 @@
 #include "utils/shell.h"
+#include "tools/clang_tidy.h"
 
 #include <iostream>
 
 using namespace linter;
 
 int main() {
-  auto [exit_code, std_out, std_err] = Execute("/linter/exe", {});
-  std::cout << exit_code << "\n";
-  std::cout << std_out;
-  std::cout << std_err;
-
-  auto [code1, std_out1, _] = Which("g++");
-  std::cout << code1 << "\n";
-  std::cout << std_out1;
+  std::cout << GetClangToolFullPath("clang-tidy", "20");
 }
