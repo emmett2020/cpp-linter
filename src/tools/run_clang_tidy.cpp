8f6e15dbc08e9170e757173c681f648dcd388a8c
root <emmettzhang2020@outlook.com>
2024-11-02 11:47:23 +0000

    fix clang-format not support c++20

--- src/tools/run_clang_tidy.cpp
+++ src/tools/run_clang_tidy.cpp
@@ -16,53 +16,59 @@
 
 namespace linter {
 
-auto AssembleVersionExec(std::string_view tool_name,
-                         std::string_view specified_version) -> std::string {
-  auto exe_path = std::string{};
-  auto semver = std::views::split(tool_name, ".");
+  auto AssembleVersionExec(std::string_view tool_name, std::string_view specified_version)
+    -> std::string {
+    auto exe_path = std::string{};
+    auto semver = std::views::split(tool_name, ".");
 
-  if (!semver.empty()) {
-    auto is_digit = [](char c) { return std::isdigit(c); };
-    if (std::ranges::all_of(semver.front(), is_digit)) {
-      auto tool_name_with_version =
-          std::format("{}-{}", tool_name, semver.front().data());
-      if (auto [ec, std_out, _] = Which(tool_name_with_version); ec == 0) {
-        return tool_name_with_version;
+    if (!semver.empty()) {
+      auto is_digit = [](char c) {
+        return std::isdigit(c);
+      };
+      if (std::ranges::all_of(semver.front(), is_digit)) {
+        auto tool_name_with_version = std::format("{}-{}", tool_name, semver.front().data());
+        if (auto [ec, std_out, _] = Which(tool_name_with_version); ec == 0) {
+          return tool_name_with_version;
+        }
       }
     }
   }
-}
 
-struct ClangTidyAdivce {};
+  struct ClangTidyAdivce { };
 
-auto run_clang_tidy(std::string_view clang_tidy_cmd_path,
-                    std::string_view file_path, std::string_view data_base_path,
-                    bool only_check_changed_line) -> ClangTidyAdivce {
-  // Make additional args
-  std::vector<std::string_view> args;
-  if (data_base_path != "") {
-    args.push_back(std::format("-p {}", data_base_path));
-  }
-  args.push_back(file_path);
+  auto run_clang_tidy(
+    std::string_view clang_tidy_cmd_path,
+    std::string_view file_path,
+    std::string_view data_base_path,
+    bool only_check_changed_line) -> ClangTidyAdivce {
+    // Make additional args
+    std::vector<std::string_view> args;
+    if (data_base_path != "") {
+      args.push_back(std::format("-p {}", data_base_path));
+    }
+    args.push_back(file_path);
 
-  auto arg_str = args | ranges::views::join(' ') | ranges::to<std::string>();
-  spdlog::info("Running {} {}", clang_tidy_cmd_path, arg_str);
+    auto arg_str = args | ranges::views::join(' ') | ranges::to<std::string>();
+    spdlog::info("Running {} {}", clang_tidy_cmd_path, arg_str);
 
-  // auto [ec, std_out, std_err] =
-  //     Exec(clang_tidy_cmd_path, {args.begin(), args.end()});
+    // auto [ec, std_out, std_err] =
+    //     Exec(clang_tidy_cmd_path, {args.begin(), args.end()});
 
-  //
-}
-
-void run_on_single_file(const std::string &file, int log_level,
-                        std::string_view clang_tidy_cmd, int db_json,
-                        std::optional<std::string> format_cmd) {
-
-  //
-  if (clang_tidy_cmd != "") {
+    //
   }
 
-  //
-}
+  void run_on_single_file(
+    const std::string &file,
+    int log_level,
+    std::string_view clang_tidy_cmd,
+    int db_json,
+    std::optional<std::string> format_cmd) {
+
+    //
+    if (clang_tidy_cmd != "") {
+    }
+
+    //
+  }
 
 } // namespace linter
