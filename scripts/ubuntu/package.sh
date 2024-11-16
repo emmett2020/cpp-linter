d41b86194a01b4570121ff0694b3eaf9f4601c09
root <emmettzhang2020@outlook.com>
2024-11-16 15:40:25 +0000

    add package

--- scripts/ubuntu/package.sh
+++ scripts/ubuntu/package.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# TODO: add this example to emmett
 CUR_SCRIPT_PATH=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
 PROJECT_PATH="${CUR_SCRIPT_PATH}/../.."
 BUILD_PATH="${PROJECT_PATH}"/build
