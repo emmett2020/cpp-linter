d58930e2a27b8cbf40922ef1777608d85fc212d9
root <emmettzhang2020@outlook.com>
2024-11-17 02:43:43 +0000

    rename directory

--- scripts/ubuntu/package.sh
+++ scripts/ubuntu/package.sh
@@ -5,6 +5,6 @@ CUR_SCRIPT_PATH=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
 PROJECT_PATH="${CUR_SCRIPT_PATH}/../.."
 BUILD_PATH="${PROJECT_PATH}"/build
 
-lddtree "${BUILD_PATH}"/main --copy-to-tree "${BUILD_PATH}"/linter --libdir /lib --bindir /bin
+lddtree "${BUILD_PATH}"/main --copy-to-tree "${BUILD_PATH}"/dist --libdir /lib --bindir /bin
 cd "${BUILD_PATH}"
 tar -cvf linter.tar.gz linter
