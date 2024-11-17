6392c4c6226788a490fe4b0ebfc79398254c4a35
root <emmettzhang2020@outlook.com>
2024-11-17 16:37:15 +0000

    add new package

--- scripts/ubuntu/package.sh
+++ scripts/ubuntu/package.sh
@@ -6,7 +6,8 @@ PROJECT_PATH="${CUR_SCRIPT_PATH}/../.."
 BUILD_PATH="${PROJECT_PATH}"/build
 
 lddtree "${BUILD_PATH}"/linter --copy-to-tree "${BUILD_PATH}"/dist --libdir /lib --bindir /bin
-patchelf --set-interpreter ../lib/ld-linux-aarch64.so.1 ${BUILD_PATH}/dist/bin/linter
+# patchelf --set-interpreter ../lib/ld-linux-aarch64.so.1 ${BUILD_PATH}/dist/bin/linter
+patchelf --set-interpreter ../lib/ld-linux-x86-64.so.2 ${BUILD_PATH}/dist/bin/linter
 
 cd "${BUILD_PATH}"
 tar -cvf dist.tar.gz dist
