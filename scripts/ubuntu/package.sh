49ab7d62fd591b72b7b0dfcf7b42cfc2403f1ac6
root <emmettzhang2020@outlook.com>
2024-11-18 01:13:24 +0000

    add version 0.0.8 for test

--- scripts/ubuntu/package.sh
+++ scripts/ubuntu/package.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# TODO: add this example to emmett
 CUR_SCRIPT_PATH=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
 PROJECT_PATH="${CUR_SCRIPT_PATH}/../.."
 BUILD_PATH="${PROJECT_PATH}"/build
 
+echo "Start to package this project"
 lddtree "${BUILD_PATH}"/linter --copy-to-tree "${BUILD_PATH}"/dist --libdir /lib --bindir /bin
-# patchelf --set-interpreter ../lib/ld-linux-aarch64.so.1 ${BUILD_PATH}/dist/bin/linter
-patchelf --set-interpreter ../lib/ld-linux-x86-64.so.2 ${BUILD_PATH}/dist/bin/linter
+patchelf --set-interpreter dist/lib/ld-linux-x86-64.so.2 ${BUILD_PATH}/dist/bin/linter
 
 cd "${BUILD_PATH}"
 tar -cvf dist.tar.gz dist
