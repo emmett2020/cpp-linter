6698bf4253b058945f607994169af749d1bba786
root <emmettzhang2020@outlook.com>
2024-11-18 07:08:07 +0000

    rename more

--- scripts/linux/ubuntu/package.sh
+++ scripts/linux/ubuntu/package.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-CUR_SCRIPT_PATH=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
-PROJECT_PATH="${CUR_SCRIPT_PATH}/../.."
-BUILD_PATH="${PROJECT_PATH}"/build
+CUR_SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
+PROJECT_ROOT_PATH="${CUR_SCRIPT_DIR}/../../.."
+PROJECT_BUILD_PATH="${PROJECT_PATH}"/build
 
-echo "Start to package this project"
-lddtree "${BUILD_PATH}"/linter --copy-to-tree "${BUILD_PATH}"/dist --libdir /lib --bindir /bin
-patchelf --set-interpreter dist/lib/ld-linux-x86-64.so.2 ${BUILD_PATH}/dist/bin/linter
+lddtree "${PROJECT_BUILD_PATH}"/cpp-linter --copy-to-tree "${PROJECT_BUILD_PATH}"/cpp-linter-dist --libdir /lib/cpp-linter --bindir /bin
+# patchelf --set-interpreter dist/lib/ld-linux-x86-64.so.2 ${PROJECT_BUILD_PATH}/dist/bin/linter
 
-cd "${BUILD_PATH}"
-tar -cvf dist.tar.gz dist
+#
+# pushd "${BUILD_PATH}"
+# tar -cvf dist.tar.gz dist
+# popd
