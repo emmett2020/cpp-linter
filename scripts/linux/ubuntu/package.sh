458aeafedf1ac80562d1d9682a1392c499a39667
root <emmettzhang2020@outlook.com>
2024-11-18 08:37:25 +0000

    add package

--- scripts/linux/ubuntu/package.sh
+++ scripts/linux/ubuntu/package.sh
@@ -15,16 +15,22 @@ DISTRIBUTION_NAME="cpp-linter-dist"
 INTERPRETER_INSTALL_PATH="~/.local/lib/${BINARY_NAME}"
 
 echo "Start to package ${BINARY_NAME}"
+echo "1. Start to collect dependent shared libraries ${BINARY_NAME}"
 lddtree "${BINARY_NAME}"  \
         --copy-to-tree "${DISTRIBUTION_NAME}" \
         --libdir "/lib/${BINARY_NAME}" \
         --bindir /bin
 
+echo "2. Start to set new interpreter path: ${INTERPRETER_INSTALL_PATH}/${interpreter}"
 interpreter=$(ls ${DISTRIBUTION_NAME}/lib/${BINARY_NAME} | grep "ld-linux")
 patchelf --set-interpreter "${INTERPRETER_INSTALL_PATH}/${interpreter}" \
                            ${DISTRIBUTION_NAME}/bin/${BINARY_NAME}
-echo "Set new interpreter path: ${INTERPRETER_INSTALL_PATH}/${interpreter}"
 
+echo "3. Start to set rpath for: ${BINARY_NAME}"
+patchelf --set-rpath '$ORIGIN/../lib/${BINARY_NAME}' "${DISTRIBUTION_NAME}/bin/${BINARY_NAME}"
+
+
+echo "4. Start to compress"
 tar -cvf ${DISTRIBUTION_NAME}.tar.gz ${DISTRIBUTION_NAME}
 popd &> /dev/null
 echo "Successfully packaged ${BINARY_NAME} and it's dependencies into ${DISTRIBUTION_NAME}"
