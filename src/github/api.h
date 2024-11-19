d0d34a510631f1d3d3a4c918c11fd2e63a7dcdc7
root <emmettzhang2020@outlook.com>
2024-11-19 16:33:27 +0000

    add more

--- src/github/api.h
+++ src/github/api.h
@@ -14,6 +14,7 @@
 #include <httplib.h>
 #include <nlohmann/json.hpp>
 #include <spdlog/spdlog.h>
+#include <utility>
 
 #include "nlohmann/json_fwd.hpp"
 #include "utils/env_manager.h"
@@ -27,38 +28,81 @@ namespace linter {
     std::size_t retry     = 0;
   };
 
-  // constexpr auto app_name                  = "linter";
-  constexpr auto our_name                  = "emmett2020"; // For test
-  constexpr auto github_api                = "https://api.github.com";
-  constexpr auto github_event_pull_request = "pull_request";
-  constexpr auto github_event_push         = "push";
+  constexpr auto our_name                         = "emmett2020"; // For test
+  constexpr auto github_api                       = "https://api.github.com";
+  constexpr auto github_event_pull_request        = "pull_request";
+  constexpr auto github_event_pull_request_target = "pull_request_target";
+  constexpr auto github_event_push                = "push";
 
   // Github Actions
   // https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables
-  constexpr auto github_ci = "CI";
+  constexpr auto github_actions    = "GITHUB_ACTIONS";
   constexpr auto github_repository = "GITHUB_REPOSITORY"; // The owner and repository name
   constexpr auto github_token      = "GITHUB_TOKEN";
   constexpr auto github_event_name = "GITHUB_EVENT_NAME";
   constexpr auto github_event_path = "GITHUB_EVENT_PATH";
-  constexpr auto github_sha        = "GITHUB_SHA";
-  constexpr auto github_ref        = "GITHUB_REF";
+
+
+  /// The default working directory on the runner for steps, and the default
+  /// location of your repository when using the checkout action. For example,
+  /// /home/runner/work/my-repo-name/my-repo-name.
+  constexpr auto github_workspace = "GITHUB_WORKSPACE";
+
+  /// The commit SHA that triggered the workflow. The value of this commit SHA
+  /// depends on the event that triggered the workflow. For more information, see
+  /// "Events that trigger workflows." For example,
+  /// ffac537e6cbbf934b08745a378932722df287a53.
+  constexpr auto github_sha = "GITHUB_SHA";
+
+  /// The name of the base ref or target branch of the pull request in a
+  /// workflow run. This is only set when the event that triggers a workflow
+  /// run is either pull_request or pull_request_target. For example, main.
+  constexpr auto github_base_ref = "GITHUB_BASE_REF";
+
+  /// The head ref or source branch of the pull request in a workflow run. This
+  /// property is only set when the event that triggers a workflow run is either
+  /// pull_request or pull_request_target. For example, feature-branch-1.
+  constexpr auto github_head_ref = "GITHUB_HEAD_REF";
+
+  /// The fully-formed ref of the branch or tag that triggered the workflow
+  /// run. For workflows triggered by push, this is the branch or tag ref that
+  /// was pushed. For workflows triggered by pull_request, this is the pull
+  /// request merge branch. For workflows triggered by release, this is the
+  /// release tag created. For other triggers, this is the branch or tag ref that
+  /// triggered the workflow run. This is only set if a branch or tag is
+  /// available for the event type. The ref given is fully-formed, meaning that
+  /// for branches the format is refs/heads/<branch_name>, for pull requests it
+  /// is refs/pull/<pr_number>/merge, and for tags it is refs/tags/<tag_name>.
+  /// For example, refs/heads/feature-branch-1
+  constexpr auto github_ref = "GITHUB_REF";
+
+  /// The type of ref that triggered the workflow run. Valid values are branch or tag
+  constexpr auto github_ref_type = "GITHUB_REF";
+
+  enum class ref_t : uint8_t {
+    branch,
+    tag,
+  };
 
   /// Reads from the actual Github runner.
   struct github_env {
     std::string repository;
     std::string event_name;
     std::string event_path;
-    std::string source_sha;
-    std::string source_ref;
+    std::string base_ref;      // Only used in pr event
+    std::string head_ref;      // Only used in pr event
+    std::string triggered_ref; // The ref triggered the workflow. Used in all events.
+    std::string triggered_sha;
+    ref_t triggered_ref_type{ref_t::branch};
+    std::string workspace;
     std::string token;
   };
 
   class github_api_client {
   public:
     github_api_client() {
-      load_envionment_variables();
       infer_configs();
-      if (github_env_.event_name == github_event_pull_request) {
+      if (env_.event_name == github_event_pull_request) {
         parse_pr_number();
       }
     }
@@ -88,12 +132,12 @@ namespace linter {
     // TODO: Maybe do it in another thread to speed up or do it in same thread to eliminate call times?
     void get_issue_comment_id() {
       spdlog::info("Start to get issue comment id for pull request: {}.", pr_number_);
-      assert(github_env_.event_name == github_event_pull_request);
+      assert(env_.event_name == github_event_pull_request);
 
-      auto path = std::format("/repos/{}/issues/{}/comments", github_env_.repository, pr_number_);
+      auto path    = std::format("/repos/{}/issues/{}/comments", env_.repository, pr_number_);
       auto headers = httplib::Headers{
         {"Accept", "application/vnd.github+json"},
-        {"Authorization", std::format("token {}", github_env_.token)}
+        {"Authorization", std::format("token {}", env_.token)}
       };
       spdlog::trace("path: {}", path);
 
@@ -127,11 +171,10 @@ namespace linter {
     void add_comment(const std::string& body) {
       spdlog::info("Start to add issue comment for pr {}", pr_number_);
 
-      const auto path =
-        std::format("/repos/{}/issues/{}/comments", github_env_.repository, pr_number_);
+      const auto path    = std::format("/repos/{}/issues/{}/comments", env_.repository, pr_number_);
       const auto headers = httplib::Headers{
         {"Accept", "application/vnd.github.use_diff"},
-        {"Authorization", std::format("token {}", github_env_.token)}
+        {"Authorization", std::format("token {}", env_.token)}
       };
       spdlog::trace("Path: {}, Body: {}", path, body);
 
@@ -151,11 +194,10 @@ namespace linter {
       throw_if(pr_number_ == -1, "doesn't have comment_id yet");
       spdlog::info("Start to update issue comment");
 
-      const auto path =
-        std::format("/repos/{}/issues/comments/{}", github_env_.repository, comment_id_);
+      const auto path = std::format("/repos/{}/issues/comments/{}", env_.repository, comment_id_);
       const auto headers = httplib::Headers{
         {"Accept", "application/vnd.github.use_diff"},
-        {"Authorization", std::format("token {}", github_env_.token)}
+        {"Authorization", std::format("token {}", env_.token)}
       };
       spdlog::trace("Path: {}, Body: {}", path, body);
 
@@ -174,18 +216,18 @@ namespace linter {
     }
 
     auto get_changed_files() -> std::vector<std::string> {
-      auto path = std::format("/repos/{}", github_env_.repository);
-      if (github_env_.event_name == github_event_pull_request) {
+      auto path = std::format("/repos/{}", env_.repository);
+      if (env_.event_name == github_event_pull_request) {
         path += std::format("/pulls/{}", pr_number_);
       } else {
-        throw_unless(github_env_.event_name == github_event_push, "unsupported event");
-        path += std::format("/commits/{}", github_env_.source_sha);
+        throw_unless(env_.event_name == github_event_push, "unsupported event");
+        path += std::format("/commits/{}", env_.triggered_sha);
       }
       spdlog::info("Fetching changed files from: {}{}", github_api, path);
       auto client  = httplib::Client{github_api};
       auto headers = httplib::Headers{
         {"Accept", "application/vnd.github.use_diff"},
-        {"Authorization", std::format("token {}", github_env_.token)}
+        {"Authorization", std::format("token {}", env_.token)}
       };
 
       auto response = client.Get(path, headers);
@@ -195,35 +237,70 @@ namespace linter {
       return {}; // parse
     }
 
-  private:
-    void load_envionment_variables() {
-      github_env_.repository = env::get(github_repository);
-      github_env_.token      = env::get(github_token);
-      github_env_.event_name = env::get(github_event_name);
-      github_env_.event_path = env::get(github_event_path);
-      github_env_.source_sha = env::get(github_sha);
-      github_env_.source_ref = env::get(github_ref);
+    [[nodiscard]] auto env() const -> const github_env& {
+      return env_;
     }
 
+    void read_envrionment_variables() {
+      env_.repository         = env::get(github_repository);
+      env_.token              = env::get(github_token);
+      env_.event_name         = env::get(github_event_name);
+      env_.event_path         = env::get(github_event_path);
+      env_.base_ref           = env::get(github_base_ref);
+      env_.head_ref           = env::get(github_head_ref);
+      env_.triggered_ref      = env::get(github_ref);
+      env_.triggered_sha      = env::get(github_sha);
+      env_.triggered_ref_type = env::get(github_ref_type) == "branch" ? ref_t::branch : ref_t::tag;
+      env_.workspace          = env::get(github_workspace);
+    }
+
+    [[nodiscard]] auto base_commit() const -> const std::string& {
+      if (env_.event_name == github_event_push) {
+        throw_if(true, "unsupported");
+      }
+      if (this->env_.event_name == github_event_pull_request) {
+        return this->env_.triggered_sha;
+      }
+
+      throw_if(true, "unsupported");
+      std::unreachable();
+    }
+
+    [[nodiscard]] auto head_commit() const -> const std::string& {
+      if (env_.event_name == github_event_push) {
+        throw_if(true, "unsupported");
+      }
+      if (this->env_.event_name == github_event_pull_request) {
+        return this->env_.triggered_sha;
+      }
+
+      throw_if(true, "unsupported");
+      std::unreachable();
+    }
+
+    [[nodiscard]] auto repo_full_path() const -> const std::string& {
+      return env_.workspace;
+    }
+
+  private:
     /// PR merge branch refs/pull/PULL_REQUEST_NUMBER/merge
     void parse_pr_number() {
-      assert(!github_env_.source_ref.empty());
-      auto parts = std::views::split(github_env_.source_ref, '/')
+      assert(!env_.head_ref.empty());
+      auto parts = std::views::split(env_.head_ref, '/')
                  | std::ranges::to<std::vector<std::string>>();
-      throw_if(parts.size() != 4,
-               std::format("source ref format error: {}", github_env_.source_ref));
+      throw_if(parts.size() != 4, std::format("source ref format error: {}", env_.head_ref));
       pr_number_ = std::stoi(parts[2]);
     }
 
     void infer_configs() {
-      if (!github_env_.event_path.empty()) {
-        auto file = std::ifstream(github_env_.event_name);
+      if (!env_.event_path.empty()) {
+        auto file = std::ifstream(env_.event_name);
         auto data = nlohmann::json::parse(file);
         data.at("number").get_to(pr_number_);
       }
     }
 
-    github_env github_env_;
+    github_env env_;
     bool enable_debug_        = false;
     std::size_t pr_number_    = -1;
     std::uint32_t comment_id_ = -1;
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <algorithm>
-#include <boost/program_options/variables_map.hpp>
 #include <cctype>
+#include <filesystem>
 #include <print>
 #include <ranges>
 #include <string>
@@ -8,7 +8,9 @@
 #include <spdlog/spdlog.h>
 #include <git2/diff.h>
 #include <boost/program_options.hpp>
+#include <boost/program_options/variables_map.hpp>
 
+#include "spdlog/common.h"
 #include "tools/clang_tidy.h"
 #include "github/api.h"
 #include "utils/git_utils.h"
@@ -17,7 +19,7 @@
 
 using namespace linter; // NOLINT
 using namespace std::string_literals;
-namespace po = boost::program_options;
+namespace program_options = boost::program_options;
 
 namespace {
   /// Find the full executable path of clang tools with specific version.
@@ -30,6 +32,8 @@ namespace {
     return std_out;
   }
 
+  constexpr auto supported_log_level = {"TRACE", "DEBUG", "INFO", "ERROR"};
+
   /// This must be called before any spdlog use.
   void set_log_level(const std::string &log_level_str) {
     auto log_level = spdlog::level::info;
@@ -37,6 +41,8 @@ namespace {
       log_level = spdlog::level::trace;
     } else if (log_level_str == "DEBUG") {
       log_level = spdlog::level::debug;
+    } else if (log_level_str == "ERROR") {
+      log_level = spdlog::level::err;
     } else {
       log_level = spdlog::level::info;
     }
@@ -47,32 +53,33 @@ namespace {
   struct user_options {
     bool use_on_local = false;
     std::string log_level;
-    std::string repo_path; // the path to repo, without owner
-    std::string repo;      // owner/repo
+    std::string repo_full_path;
+    std::string owner_and_repo;
     std::string target_ref;
     std::string source_sha;
     std::string source_ref;
+    std::string token;
 
-    bool enable_clang_tidy;
-    bool clang_tidy_fast_exit;
+    bool enable_clang_tidy           = true;
+    bool enable_clang_tidy_fast_exit = false;
     std::string clang_tidy_version;
     clang_tidy::option clang_tidy_option;
   };
 
   void print_full_options(const user_options &options) {
     spdlog::debug("Log level: {}", options.log_level);
-    spdlog::debug("Repository path: {}", options.repo_path);
-    spdlog::debug("Repository: {}", options.repo);
-    spdlog::debug("Repository full path: {}/{}", options.repo_path, options.repo);
+    spdlog::debug("Repository path: {}", options.repo_full_path);
+    spdlog::debug("Repository: {}", options.owner_and_repo);
+    spdlog::debug("Repository full path: {}/{}", options.repo_full_path, options.owner_and_repo);
     spdlog::debug("Repository target ref: {}", options.target_ref);
     spdlog::debug("Repository source ref: {}", options.source_ref);
     spdlog::debug("Repository source sha: {}", options.source_sha);
 
     const auto &tidy_option = options.clang_tidy_option;
     spdlog::debug("The options of clang-tidy:");
-    spdlog::debug("clang tidy enabled: {}", options.enable_clang_tidy);
+    spdlog::debug("enable clang tidy: {}", options.enable_clang_tidy);
+    spdlog::debug("enable clang tidy fast exit: {}", options.enable_clang_tidy_fast_exit);
     spdlog::debug("clang tidy version: {}", options.clang_tidy_version);
-    spdlog::debug("clang tidy fast exit: {}", options.clang_tidy_fast_exit);
     spdlog::debug("checks: {}", tidy_option.checks);
     spdlog::debug("config: {}", tidy_option.config);
     spdlog::debug("config file: {}", tidy_option.config_file);
@@ -90,83 +97,153 @@ namespace {
     }
   }
 
-  /// WARN: Theses just for debug
-  void set_pull_request_debug_env(user_options &options) {
-    env::set_cache(github_ci, "false");
-    env::set_cache(github_repository, "emmett2020/temp");
-    env::set_cache(github_event_name, github_event_pull_request);
-    env::set_cache(github_sha, "");
-    env::set_cache(github_ref, "refs/pull/7/merge");
-    env::set_cache(github_token, "");
-
-    options.log_level                     = "TRACE";
-    options.enable_clang_tidy             = false;
-    options.clang_tidy_fast_exit          = false;
-    options.clang_tidy_version            = "20";
-    options.clang_tidy_option.config_file = ".clang-tidy";
-    options.clang_tidy_option.database    = "build";
-    options.target_ref                    = "refs/heads/main";
+  auto get_current_version() -> std::string {
+    auto file = std::ifstream{"VERSION"};
+    throw_if(file.bad(), "Open VERSION file to read failed");
+    auto buffer = std::string{};
+    std::getline(file, buffer);
+    auto trimmed_version = linter::trim(buffer);
+    throw_if(buffer.empty(), "VERSION file is empty");
+    return {trimmed_version.data(), trimmed_version.size()};
   }
 
-  /// Load user options from github.
-  auto load_user_options() -> user_options {
-    auto options = user_options{};
-    set_pull_request_debug_env(options);
-    options.use_on_local = env::get(github_ci) == "false";
-    options.repo       = env::get(github_repository);
-    options.source_sha = env::get(github_sha);
-    options.source_ref = env::get(github_ref);
+  //
+  auto parse_command_options(const program_options::variables_map &variables) -> user_options {
+    auto options         = user_options();
+    options.use_on_local = env::get(github_actions) == "true";
+
+    if (variables.contains("log_level")) {
+      options.log_level = variables["log_level"].as<std::string>();
+      throw_unless(std::ranges::contains(supported_log_level, options.log_level),
+                   "unsupported log level");
+    }
+
+    /// REFINE:
+    if (variables.contains("repo_full_path")) {
+      throw_unless(options.use_on_local, "repo_full_path option only supports on local use");
+      options.repo_full_path = variables["repo_full_path"].as<std::string>();
+    }
+
+    if (variables.contains("target_ref")) {
+      options.target_ref = variables["target_ref"].as<std::string>();
+    }
+    if (variables.contains("source_ref")) {
+      options.source_ref = variables["source_ref"].as<std::string>();
+    }
+    if (variables.contains("source_sha")) {
+      options.source_sha = variables["source_sha"].as<std::string>();
+    }
+    if (variables.contains("token")) {
+      options.token = variables["toekn"].as<std::string>();
+    }
+    if (variables.contains("enable_clang_tidy")) {
+      options.enable_clang_tidy = variables["enable_clang_tidy"].as<bool>();
+    }
+    if (variables.contains("enable_clang_tidy_fast_exit")) {
+      options.enable_clang_tidy_fast_exit = variables["enable_clang_tidy_fast_exit"].as<bool>();
+    }
+    if (variables.contains("clang_tidy_version")) {
+      options.clang_tidy_version = variables["clang_tidy_version"].as<std::string>();
+    }
+    if (variables.contains("clang_tidy_allow_no_checks")) {
+      options.clang_tidy_option.allow_no_checks =
+        variables["clang_tidy_allow_no_checks"].as<bool>();
+    }
+    if (variables.contains("clang_tidy_enable_check_profile")) {
+      options.clang_tidy_option.enable_check_profile =
+        variables["clang_tidy_enable_check_profile"].as<bool>();
+    }
+    if (variables.contains("clang_tidy_checks")) {
+      options.clang_tidy_option.checks = variables["clang_tidy_checks"].as<std::string>();
+    }
+    if (variables.contains("clang_tidy_config")) {
+      options.clang_tidy_option.config = variables["config"].as<std::string>();
+    }
+    if (variables.contains("clang_tidy_config_file")) {
+      options.clang_tidy_option.config_file = variables["config_file"].as<std::string>();
+    }
+    if (variables.contains("clang_tidy_databse")) {
+      options.clang_tidy_option.database = variables["clang_tidy_databse"].as<std::string>();
+    }
+    if (variables.contains("clang_tidy_header_filter")) {
+      options.clang_tidy_option.header_filter =
+        variables["clang_tidy_header_filter"].as<std::string>();
+    }
+    if (variables.contains("clang_tidy_line_filter")) {
+      options.clang_tidy_option.line_filter = variables["clang_tidy_line_filter"].as<std::string>();
+    }
+
     return options;
+  }
+
+  void add_supported_option(program_options::options_description &desc) {
+    desc.add_options()                                                                           //
+      ("help", "produce help message")                                                           //
+      ("version", "print current version")                                                       //
+      ("log_level", "Set the log level of cpp-linter")                                           //
+      ("repo_full_path",
+       "Set the full path of to be checked git repository. This option shouldn't be used on CI") //
+      ("target_ref", "Set the target reference of git repository")                               //
+      ("source_ref", "Set the source reference of git repository")                               //
+      ("source_sha", "Set the source sha1 of git repository")                                    //
+      ("token", "Set github token of git repository")                                            //
+      ("enable_clang_tidy", "Enabel clang-tidy check")                                           //
+      ("enable_clang_tidy_fast_exit",
+       "Enabel clang-tidy fast exit. This means cpp-linter will immediately stop all clang-tidy "
+       "check when found first check error")                                                     //
+      ("clang_tidy_version", "The version of clang-tidy")                                        //
+      ("clang_tidy_allow_no_checks", "Enabel clang-tidy allow_no_check option");
   }
 
 
 } // namespace
 
+auto main(int argc, char **argv) -> int {
+  auto desc      = program_options::options_description{"cpp-linter options"};
+  auto variables = program_options::variables_map{};
 
-int main(int argc, char** argv) {
-  // deal with the user input
-  auto desc = po::options_description{"Allowed options"};
-  desc.add_options()
-    ("help", "produce help message")
-    ("version", "show version")
-  ;
-  auto vm =  po::variables_map{};
-  po::store(po::parse_command_line(argc, argv, desc), vm);
-  po::notify(vm);
+  add_supported_option(desc);
+  program_options::store(program_options::parse_command_line(argc, argv, desc), variables);
+  program_options::notify(variables);
 
-  if (vm.count("help") != 0U) {
+  // These options don't any real work.
+  if (variables.contains("help")) {
     std::cout << desc << "\n";
-    return 1;
+    return 0;
+  }
+  if (variables.contains("version")) {
+    std::print("{}", get_current_version());
+    return 0;
   }
 
-  auto options = load_user_options();
+  auto options = parse_command_options(variables);
   set_log_level(options.log_level);
   print_full_options(options);
-  const auto repo_full_path = options.repo_path + "/" + options.repo;
-
-  git::setup();
-  auto *repo = git::repo::open(repo_full_path);
-  // auto changed_files = git::diff::changed_files(repo, options.target_ref, options.source_ref);
-  // print_changed_files(changed_files);
 
   auto github_client = github_api_client{};
-  github_client.get_issue_comment_id();
-  github_client.add_or_update_comment(R"({"body": "hello1"})");
+  github_client.read_envrionment_variables();
+  auto repo_path = options.use_on_local ? options.repo_full_path : github_client.repo_full_path();
 
-  // if (options.enable_clang_tidy) {
-  //   auto clang_tidy_exe = find_clang_tool_exe_path("clang-tidy", options.clang_tidy_version);
-  //   spdlog::info("The clang-tidy executable path: {}", clang_tidy_exe);
-  //   throw_if(clang_tidy_exe.empty(), "find clang tidy executable failed");
-  //
-  //   for (const auto &file: changed_files) {
-  //     auto result =
-  //       clang_tidy::run(clang_tidy_exe, options.clang_tidy_option, repo_full_path, file);
-  //     if (!result.pass && options.clang_tidy_fast_exit) {
-  //       spdlog::info("fast exit");
-  //       return -1;
-  //     }
-  //   }
-  // }
+  git::setup();
+  auto *repo = git::repo::open(repo_path);
+
+  auto changed_files =
+    git::diff::changed_files(repo, github_client.base_commit(), github_client.head_commit());
+  print_changed_files(changed_files);
+
+  if (options.enable_clang_tidy) {
+    auto clang_tidy_exe = find_clang_tool_exe_path("clang-tidy", options.clang_tidy_version);
+    spdlog::info("The clang-tidy executable path: {}", clang_tidy_exe);
+    throw_if(clang_tidy_exe.empty(), "find clang tidy executable failed");
+
+    for (const auto &file: changed_files) {
+      auto result = clang_tidy::run(clang_tidy_exe, options.clang_tidy_option, repo_path, file);
+      if (!result.pass && options.enable_clang_tidy_fast_exit) {
+        spdlog::info("fast exit");
+        return -1;
+      }
+    }
+  }
 
   git::repo::free(repo);
   git::shutdown();
