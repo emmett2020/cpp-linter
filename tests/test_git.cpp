cd87156842070e0422b82eab259c688e885ad912
emmettzhang <emmettzhang2020@outlook.com>
2024-11-21 11:15:32 +0000

    add unittest

--- tests/test_git.cpp
+++ tests/test_git.cpp
@@ -1,14 +1,36 @@
 #include <cctype>
+#include <filesystem>
 #include <git2/diff.h>
 #include <print>
 
 #include <spdlog/spdlog.h>
+#include <catch2/catch_all.hpp>
 
+#include "catch2/catch_test_macros.hpp"
 #include "utils/git_utils.h"
 
 using namespace linter; // NOLINT
 using namespace std::string_literals;
 
-int main() {
-  return 0;
+TEST_CASE("repo", "[git2][basic]") {
+  //
+  auto temp_dir = std::filesystem::temp_directory_path();
+  auto temp_repo_dir = temp_dir / "test_git";
+  if (std::filesystem::exists(temp_repo_dir)) {
+    std::filesystem::remove(temp_repo_dir);
+  }
+  std::filesystem::create_directory(temp_repo_dir);
+  std::print("{}", temp_dir.string());
+
+  // SECTION("init a new repo") {
+  //   git::repo::init();
+  //
+  // }
+  //
+  // SECTION("open") {
+  //   git::repo::open(const std::string &repo_path);
+  //
+  // }
+
+  std::filesystem::remove(temp_repo_dir);
 }
