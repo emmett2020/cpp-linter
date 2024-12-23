3c8770eadd0f861955963eb46492f5355e4dd699
root <emmettzhang2020@outlook.com>
2024-12-23 16:09:36 +0000

    remove useless file

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# This is the entrypoint of GITHUB action.
-
-# `$#` expands to the number of arguments and `$@` expands to the supplied `args`
-printf '%d args:' "$#"
-printf " '%s'" "$@"
-printf '\n'
-/usr/local/bin/cpp-linter
